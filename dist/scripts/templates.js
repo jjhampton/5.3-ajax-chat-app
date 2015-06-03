e988684ac2b471b2e37c3c538706397f6796a842
John Hampton <john_j_hampton@yahoo.com>
2015-06-03 13:39:30 -0400

    Added render functions and behavior to load login or chat screen in response to events

--- dist/scripts/templates.js
+++ dist/scripts/templates.js
@@ -2,3 +2,9 @@ this["JST"] = this["JST"] || {};
 this["JST"]["application"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     return "<h1>Test</h1>\n";
 },"useData":true});
+this["JST"]["chat"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    return "<h1>Chat Room</h1>\n";
+},"useData":true});
+this["JST"]["login"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    return "<form class=\"login-form\">\n  <input class=\"login-form-username\" type=\"text\" placeholder=\"Chat username\">\n  <input type=\"submit\" value=\"Login Here\">\n</form>\n";
+},"useData":true});
--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,45 @@
 (function(){
   'use strict';
 
+  var username = ''; // user will enter this into login screen
+
+
   $(document).ready(function(){
-    $(".login-form").on("submit", function(event) {
+
+    routeUser();
+
+    $(document).on('submit', '.login-form', function(event) {
       event.preventDefault();
-      window.location.hash = "/chat";
+      username = $(this).find('.login-form-username').val();
+      window.location.hash = '/chat';
     });
 
-    $(window).on("hashchange", function(event) {
-        //event.preventDefault();
-        console.log(window.location.hash)
+    $(window).on('hashchange', function(event) {
+        //event.preventDefault(); Not needed? No default behavior for hashchange event?
+        routeUser();
     });
 
   });
+
+  function routeUser() {
+    switch(window.location.hash) {
+      case '':
+        renderLogin();
+        break;
+      case '#/chat':
+        renderChat();
+        break;
+    }
+  }
+
+  function renderLogin() {
+    $('.application').html(JST['login']());
+    console.log('render login called');
+  }
+
+  function renderChat() {
+    $('.application').html(JST['chat']());
+    console.log('username:' + username);
+  }
 
 })();
