1b3fa99ab5dfb4b50ed81819744cb444c9af09a6
John Hampton <john_j_hampton@yahoo.com>
2015-06-04 01:58:29 -0400

    Add function getGravatar and MD5 library function to generate image URL Add gravatar property to POST request based on user's email entered at login prompt

--- dist/scripts/templates.js
+++ dist/scripts/templates.js
@@ -3,10 +3,10 @@ this["JST"]["application"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1
     return "<h1>Test</h1>\n";
 },"useData":true});
 this["JST"]["chat"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-    return "<h1>Chat Room</h1>\n<section class=\"message-list\">\n</section>\n<form class=\"message-form\">\n  <textarea class=\"message-form-textarea\" name=\"message\" placeholder=\"Enter your message here...\"></textarea>\n  <input type=\"submit\" value=\"Submit message\">\n</form>\n";
+    return "<section class=\"message-list\">\n</section>\n<form class=\"message-form\">\n  <textarea class=\"message-form-textarea\" name=\"message\" placeholder=\"\"></textarea>\n  <input type=\"submit\" value=\"Submit message\">\n</form>\n";
 },"useData":true});
 this["JST"]["login"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-    return "<h1>Login Area</h1>\n<input class=\"login-form-username\" type=\"text\" placeholder=\"Enter username\">\n<form class=\"login-form\">\n  <h2>General Class Chatroom</h2>\n  <input class=\"login-form-submit\" type=\"submit\" value=\"Login to General Chatroom Here\">\n</form>\n<form class=\"newroom-login-form\">\n  <h2>Other Chatrooms</h2>\n  <p>You can enter other chatrooms too.  Enter the chat room name below to join an already existing chatroom.  To create a chat room that does not already exist, enter its name below and the new room will be created when you login.\n  </p>\n  <input class=\"login-form-roomname\" type=\"text\" placeholder=\"Enter chat room name here\">\n  <input class=\"login-form-submit\" type=\"submit\" value=\"Login to your chatroom here\">\n</form>\n";
+    return "<h1>Login Area</h1>\n<input class=\"login-form-username\" type=\"text\" placeholder=\"Enter username\">\n<input class=\"login-form-email\" type=\"email\" placeholder=\"Enter email address for Gravatar\">\n<form class=\"login-form\">\n  <h2>General Class Chatroom</h2>\n  <input class=\"login-form-submit\" type=\"submit\" value=\"Login to General Chatroom Here\">\n</form>\n<form class=\"newroom-login-form\">\n  <h2>Other Chatrooms</h2>\n  <p>You can enter other chatrooms too.  Enter the chat room name below to join an already existing chatroom.  To create a chat room that does not already exist, enter its name below and the new room will be created when you login.\n  </p>\n  <input class=\"login-form-roomname\" type=\"text\" placeholder=\"Enter chat room name here\">\n  <input class=\"login-form-submit\" type=\"submit\" value=\"Login to your chatroom here\">\n</form>\n";
 },"useData":true});
 this["JST"]["message"] = Handlebars.template({"1":function(depth0,helpers,partials,data) {
     var helper, alias1=helpers.helperMissing, alias2="function", alias3=this.escapeExpression;
@@ -23,7 +23,10 @@ this["JST"]["message"] = Handlebars.template({"1":function(depth0,helpers,partia
     + alias3(((helper = (helper = helpers.roomname || (depth0 != null ? depth0.roomname : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"roomname","hash":{},"data":data}) : helper)))
     + "</li>\n</ul>\n";
 },"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-    var stack1;
+    var stack1, helper;
 
-  return ((stack1 = helpers.each.call(depth0,(depth0 != null ? depth0.messages : depth0),{"name":"each","hash":{},"fn":this.program(1, data, 0),"inverse":this.noop,"data":data})) != null ? stack1 : "");
+  return "<h1>"
+    + this.escapeExpression(((helper = (helper = helpers.roomname || (depth0 != null ? depth0.roomname : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0,{"name":"roomname","hash":{},"data":data}) : helper)))
+    + " chatroom</h1>\n"
+    + ((stack1 = helpers.each.call(depth0,(depth0 != null ? depth0.messages : depth0),{"name":"each","hash":{},"fn":this.program(1, data, 0),"inverse":this.noop,"data":data})) != null ? stack1 : "");
 },"useData":true});
--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,7 @@
 
   var username = ''; // user will enter this into login form
   var roomName = 'messages';
+  var userEmail; //for getting Gravatar
   var messageUpdateInterval;
 
 
@@ -10,26 +11,35 @@
 
     routeUser();
 
+
+    //add event handler to submit event for main chat room login
     $(document).on('submit', '.login-form', function(event) {
       //prevent entire page from reloading
       event.preventDefault();
+      //get username from HTML login form
       username = $('.login-form-username').val();
       //save username in Local Storage in case of page refresh
+      userEmail = $('.login-form-email').val();
       localStorage.setItem('loggedInUsername', username);
+      //change hash property to /chat to assist with view change
       window.location.hash = '/chat';
     });
 
+    //add event handler to submit event for other chat rooms
     $(document).on('submit', '.newroom-login-form', function(event) {
       //prevent entire page from reloading
       event.preventDefault();
       username = $('.login-form-username').val();
+      userEmail = $('.login-form-email').val();
       roomName = $(this).find('.login-form-roomname').val();
       console.log(roomName);
       //save username in Local Storage in case of page refresh
       localStorage.setItem('loggedInUsername', username);
+      //change hash property to /chat to assist with view change
       window.location.hash = '/newroom';
     });
 
+    //adds event handler to submit event for message form on chat page
     $(document).on('submit', '.message-form', function(event) {
       var messageText;
 
@@ -39,6 +49,7 @@
       $(this).find('.message-form-textarea').val("");
     });
 
+    //adds event handler to hash property change - will happen after chat room logins submitted
     $(window).on('hashchange', function(event) {
         //event.preventDefault(); Not needed? No default behavior for hashchange event?
         routeUser();
@@ -46,6 +57,7 @@
 
   });
 
+  //after hash change, routes window location to functions which will display different views
   function routeUser() {
     switch(window.location.hash) {
       case '':
@@ -60,21 +72,28 @@
     }
   }
 
+  //sets .application <div> contents to login template
   function renderLogin() {
     $('.application').html(JST['login']());
   }
 
+  //sets .application <div> to chat template and performs other tasks to manage running of chat view
   function renderChat(roomName) {
-    $('.application').html(JST['chat']());
+    $('.application').html(JST['chat']()); //adds chat template to page
+
+    //checks to see if interval is running
     if (typeof messageUpdateInterval !== "undefined") {
       console.log("clear condition called");
+      //clears interval
       clearInterval(messageUpdateInterval);
     }
 
+    // GET request for messages from designated server roomname, data recieved piped into displayMessages
     $.ajax({
       url: "http://tiny-lasagna-server.herokuapp.com/collections/" + roomName + "/"
     }).then(displayMessages);
 
+    // sets interval to perform GET request above every 30 seconds, assigns to variable to enable clearing of interval later
     messageUpdateInterval = setInterval(function() {
       $.ajax({
         url: "http://tiny-lasagna-server.herokuapp.com/collections/" + roomName + "/"}).then(displayMessages);
@@ -82,22 +101,24 @@
     console.log("setinterval with" + roomName);
   }
 
-
-  //Send AJAX
+  //adds message template w/ context to message list within chat template already on HTML page
   function displayMessages(messages) {
     console.log(messages);
-    $('.message-list').html(JST['message']({messages}));
+    $('.message-list').html(JST['message']({
+      messages: messages,
+      roomname: roomName
+      }));
   }
 
+  //event handler function
   function addMessage(message) {
-
-
     var messageObject = {
       //retrieve username from Local Storage
       username: localStorage.getItem("loggedInUsername"),
       created_at: new Date(),
       content: message,
-      roomname: roomName
+      roomname: roomName,
+      gravatar: getGravatar(userEmail)
     };
 
     $.ajax({
@@ -109,13 +130,22 @@
         url: "http://tiny-lasagna-server.herokuapp.com/collections/" + roomName + "/"
       }).then(displayMessages);
     });
-
-
   }
 
   Handlebars.registerHelper('moment', function(date){
       var result = moment(date).format('h:mma, ddd D MMMM YYYY');
       return new Handlebars.SafeString(result);
-    });
+  });
+
+  function getGravatar(email) {
+    console.log(email);
+    var imgURL;
+    // var newEmail = email.toString().trim().toLowerCase();
+
+    var hash = CryptoJS.MD5(email).toString();
+    console.log(hash);
+    imgURL = "http://gravatar.com/avatar/" + hash;
+    return imgURL;
+  }
 
 })();
